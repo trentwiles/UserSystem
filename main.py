3cafb7bda593e3427fd9d6c03a5216cebefb260c
Riverside Rocks <trent@riverside.rocks>
2020-07-29 08:56:21 -0400

    Update message

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return 'Hello world'
 
+@app.route('/RiversideRocks')
+def cakes():
+    return 'Look its Riverside Rocks!'
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
