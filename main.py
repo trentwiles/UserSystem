8dae67e324629ed86b4f6d5a6058fd5d17f9d048
Riverside Rocks <trent@riverside.rocks>
2020-07-29 10:09:41 -0400

    Try to parse JSON

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from flaskext.mysql import MySQL
 import pymysql
+import json
+
 
 db = pymysql.connect("localhost", "root", "", "python")
 
@@ -19,7 +21,9 @@ def hello(name):
     sql = "SELECT * FROM users WHERE Username='Tucker'"
     cursor.execute(sql)
     results = cursor.fetchall()
-    return render_template('index.html', results=results, name=name)
+    sql = json.loads(str(results))
+    user = sql[1]
+    return render_template('index.html', user=user, name=name)
 
 
 if __name__ == '__main__':
