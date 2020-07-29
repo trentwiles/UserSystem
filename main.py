64b8dfae0176bf4d9537fcd3e44b4989940ff89e
Riverside Rocks <trent@riverside.rocks>
2020-07-29 09:10:34 -0400

    Added SQL

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flaskext.mysql import MySQL
+import pymysql
+
+db = pymysql.connect("localhost", "root", "", "python")
+api = Api(app)
+
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -8,7 +15,13 @@ def index():
 
 @app.route('/user/<name>')
 def hello(name):
-    return render_template('index.html', name=name)
+    name=name
+    cursor = db.cursor()
+    sql = "SELECT * FROM table WHERE Username=name"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    return render_template('index.html', results=results)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
